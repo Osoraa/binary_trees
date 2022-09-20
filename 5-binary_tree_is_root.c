598925650f59685cb11ef69e2c393d2000f653d7
Osoraa <sochi98.sn@gmail.com>
2022-09-20 13:18:23 +0100

    Task 5 - Is root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_is_root - Checks if a node is the root.
  * @node: Node to check if root.
- * 
+ *
  * Return: 1 if node is root, 0 otherwise
  */
 int binary_tree_is_root(const binary_tree_t *node)
