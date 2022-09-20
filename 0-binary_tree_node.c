691dc1c2d79db6d55abc3fab2026bd091682b479
Osoraa <sochi98.sn@gmail.com>
2022-09-20 08:24:26 +0100

    Task 0 - New node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,10 +13,13 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *node;
 
 	node = malloc(sizeof(binary_tree_t));
+	if (!node)
+		return (NULL);
+
+	node->parent = parent;
 	node->n = value;
 	node->left = NULL;
 	node->right = NULL;
-	node->parent = parent;
 
 	return (node);
 }
